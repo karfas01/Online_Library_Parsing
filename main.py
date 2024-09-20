953eb1c30d01f41edc814d4baaafcf2d458e2c2c
karfas <80702492+karfas01@users.noreply.github.com>
2024-09-20 19:13:19 +0300

    added a redirect check
    added the check_for_redirect function

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ from pathlib import Path
 import requests
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError()
+
+
 for id in range(1, 11):
     url = f"https://tululu.org/txt.php?id={id}"
 
-    response = requests.get(url)
-    response.raise_for_status()
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        check_for_redirect(response)
 
-    folder_name = Path("books_db")
-    file_name = folder_name / f"book_{id}.txt"
+        folder_name = Path("books_db")
+        file_path = f"{folder_name}/book_{id}.txt"
 
-    if not folder_name.exists():
-        folder_name.mkdir()
+        if not folder_name.exists():
+            folder_name.mkdir()
 
-    with open(file_name, 'wb') as file:
-        file.write(response.content)
-
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+    except:
+        print("Такой книги нет")
