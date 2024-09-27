e56dc361f5095e4b9defbe97e6707dca5aeeb1da
karfas <80702492+karfas01@users.noreply.github.com>
2024-09-27 20:15:11 +0300

    download books with titles.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from pathlib import Path
 import requests
 
@@ -6,22 +7,39 @@ def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError()
 
+def download_books(folder_db_name, book_name, download_response):
+    file_path = f"{folder_db_name}/{book_name}.txt"
+
+    with open(file_path, 'wb') as file:
+            file.write(download_response.content)
+
 
 for id in range(1, 11):
-    url = f"https://tululu.org/txt.php?id={id}"
+    download_page_url = f"https://tululu.org/txt.php"
+    pages_book_url = f"https://tululu.org/b{id}/"
 
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
+        params = {
+        "id":id,
+        }
 
-        folder_name = Path("books_db")
-        file_path = f"{folder_name}/book_{id}.txt"
+        download_response = requests.get(download_page_url, params)
+        download_response.raise_for_status()
+        check_for_redirect(download_response)
 
-        if not folder_name.exists():
-            folder_name.mkdir()
+        response_book_pages = requests.get(pages_book_url)
+        soup = BeautifulSoup(response_book_pages.text, 'lxml')
+        title_tag = soup.find('td', class_='ow_px_td').find('h1')
+        title_text = title_tag.text
+        title_split_text = title_text.split(" :: ")
+        book_name = f"{title_split_text[0].strip()}"
+        book_author = f"{title_split_text[1].strip()}"
 
-        with open(file_path, 'wb') as file:
-            file.write(response.content)
+        folder_db_name = Path("books_db")
+        if not folder_db_name.exists():
+            folder_db_name.mkdir()
+        
+        download_books(folder_db_name, book_name, download_response)
+
     except:
         print("Такой книги нет")
