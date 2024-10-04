94134790b05bc855ef3b320d28f7638def15dbb0
karfas <80702492+karfas01@users.noreply.github.com>
2024-10-04 20:02:44 +0300

    download book covers.

--- main.py
+++ main.py
@@ -7,39 +7,56 @@ def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError()
 
-def download_books(folder_db_name, book_name, download_response):
-    file_path = f"{folder_db_name}/{book_name}.txt"
-
+def download_books(folder_name, book_name, response):
+    file_path = f"{folder_name}/{book_name}.txt"
     with open(file_path, 'wb') as file:
-            file.write(download_response.content)
+            file.write(response.content)
+
+def download_images(folder_name, image_name, response):
+    file_path = f"{folder_name}/{image_name}"
+    with open(file_path, 'wb') as file:
+            file.write(response.content)
 
 
 for id in range(1, 11):
-    download_page_url = f"https://tululu.org/txt.php"
-    pages_book_url = f"https://tululu.org/b{id}/"
+    book_url = f"https://tululu.org/txt.php"
+    page_url = f"https://tululu.org/b{id}/"
 
     try:
         params = {
-        "id":id,
+            "id":id,
         }
 
-        download_response = requests.get(download_page_url, params)
-        download_response.raise_for_status()
-        check_for_redirect(download_response)
+        book_response = requests.get(book_url, params)
+        book_response.raise_for_status()
+        check_for_redirect(book_response)
 
-        response_book_pages = requests.get(pages_book_url)
-        soup = BeautifulSoup(response_book_pages.text, 'lxml')
+        response_page = requests.get(page_url)
+        soup = BeautifulSoup(response_page.text, 'lxml')
+
+        img_url = soup.find('div', class_="bookimage").find('img')['src']
+        image_name = img_url.split("/")[-1]
+        img_url = f"https://tululu.org{img_url}"
+
+        image_response = requests.get(img_url)
+        image_response.raise_for_status()
+        folder_name = Path("images")
+        if not folder_name.exists():
+            folder_name.mkdir()
+        download_images(folder_name, image_name, image_response)
+
         title_tag = soup.find('td', class_='ow_px_td').find('h1')
         title_text = title_tag.text
         title_split_text = title_text.split(" :: ")
         book_name = f"{title_split_text[0].strip()}"
         book_author = f"{title_split_text[1].strip()}"
 
-        folder_db_name = Path("books_db")
-        if not folder_db_name.exists():
-            folder_db_name.mkdir()
+        folder_book_name = Path("books")
+        if not folder_book_name.exists():
+            folder_book_name.mkdir()
         
-        download_books(folder_db_name, book_name, download_response)
+        download_books(folder_book_name, book_name, book_response)
+        
 
     except:
         print("Такой книги нет")
